a03b1cc71b7619bd4c10daa16da21c08416e700a
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-06-23 14:14:41 -0700

    pass test to increment a plant water by 1

--- __tests__/plant.test.js
+++ __tests__/plant.test.js
@@ -1,9 +1,9 @@
-import { feed, blueFood } from './../src/plant.js';
+import { feed, blueFood, hydrate, giveLight, greenFood, yuckyFood } from './../src/plant.js';
 
 describe('feed', () => {
   test('should increment a plant soil by 1', () => {
     const plant = {};
-    const newPlant = feed(1)(plant)
+    const newPlant = feed(1)(plant);
     expect(newPlant.soil).toEqual(1);
   });
 });
@@ -11,7 +11,15 @@ describe('feed', () => {
 describe('blueFood', () => {
   test('should increment a plant soil by 5', () => {
     const plant = {};
-    const newPlant = blueFood(plant)
+    const newPlant = blueFood(plant);
     expect(newPlant.soil).toEqual(5);
+  });
+});
+
+describe('hydrate', () => {
+  test('should increment a plant water by 1', () => {
+    const plant = {};
+    const newPlant = hydrate(1)(plant);
+    expect(newPlant.water).toEqual(1);
   });
 });
